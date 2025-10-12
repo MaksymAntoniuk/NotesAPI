7eaa17689b6649a1922a37dab6ec9505e66e9d8b
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-12 23:37:50 +0200

    Add data generation helper

--- src/main/java/io/maksym/web/Helpers/DataGenerators.java
+++ src/main/java/io/maksym/web/Helpers/DataGenerators.java
@@ -1,4 +1,34 @@
 package io.maksym.web.Helpers;
 
+import com.github.javafaker.Faker;
+
 public class DataGenerators {
+    String fakeName;
+    public String generateValidRandomName(int minLength, int maxLength) {
+        do{
+            if(minLength > 30){
+                fakeName = new Faker().name().lastName() + new Faker().name().name() + new Faker().name().name();
+            }
+            else {
+                fakeName = new Faker().name().firstName();
+            }
+            System.out.println(fakeName);
+        } while((fakeName.length() < minLength || fakeName.length() > maxLength));
+        return fakeName;
+    }
+
+    public String generateRandomEmail(boolean isValid) {
+       if (!isValid){
+           return "invalidEmail";
+       }
+           return new Faker().artist().name().replaceAll("\\s+", "_") + System.currentTimeMillis() + "@gmail.com";
+    }
+
+    public String generateRandomPassword(int minLength, int maxLength) {
+        return new Faker().internet().password(minLength, maxLength);
+    }
+//    public String generateRandomName(int minLength, int maxLength) {
+//        return new Faker().name().firstName().replaceAll("\\s+", "_");
+//    }
+
 }
