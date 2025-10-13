96128700266a26fc17da9351506006bd0e220f48
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-13 23:48:55 +0200

    Improve methods in BaseRequestLibrary

--- src/main/java/io/maksym/web/library/BaseRequestLibrary.java
+++ src/main/java/io/maksym/web/library/BaseRequestLibrary.java
@@ -1,4 +1,15 @@
 package io.maksym.web.library;
 
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
 public class BaseRequestLibrary {
+    public static RequestSpecification getRequestSpec() {
+        return given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .log().all();
+    }
 }
