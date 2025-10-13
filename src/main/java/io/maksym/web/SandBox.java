bcefe8317619759e884a7252a8067ed72f6126ee
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-13 23:48:54 +0200

    Update SandBox class for testing improvements

--- src/main/java/io/maksym/web/SandBox.java
+++ src/main/java/io/maksym/web/SandBox.java
@@ -1,4 +1,9 @@
 package io.maksym.web;
 
+import static io.maksym.web.enums.StatusCode.CREATED_STATUS;
+
 public class SandBox {
+    public static void main(String[] args) {
+        System.out.println(CREATED_STATUS);
+    }
 }
