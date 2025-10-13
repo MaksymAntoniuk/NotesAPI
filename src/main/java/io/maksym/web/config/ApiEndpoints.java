a023ac0edf78e9d2ff0d35d7e075221aa19a0cec
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-13 23:48:54 +0200

    Update API endpoints configuration

--- src/main/java/io/maksym/web/config/ApiEndpoints.java
+++ src/main/java/io/maksym/web/config/ApiEndpoints.java
@@ -1,4 +1,11 @@
 package io.maksym.web.config;
 
-public class ApiEndpoints {
+public final class ApiEndpoints {
+    private ApiEndpoints() {
+    }
+
+    public static final String BASE_URL = "https://practice.expandtesting.com/notes/api/";
+    public static final String ENDPOINT_CREATE_USER = "users/register";
+    public static final String ENDPOINT_HEALTH_CHECK = "health-check";
+    public static final String ENDPOINT_LOG_IN = "users/login";
 }
