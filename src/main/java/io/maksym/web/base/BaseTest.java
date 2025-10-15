5b917ea3011c4d28901908ff04a074772bd2f3d4
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-15 23:19:01 +0200

    Implement SimpleAction in BaseTest to follow new structure

--- src/main/java/io/maksym/web/base/BaseTest.java
+++ src/main/java/io/maksym/web/base/BaseTest.java
@@ -1,27 +1,32 @@
 package io.maksym.web.base;
 
 import io.maksym.web.config.ApiEndpoints;
+import io.maksym.web.dto.Login.LoginResponse;
+import io.maksym.web.dto.Registration.RegistrationSuccResponse.RegistrationSuccessfulResponse;
 import io.maksym.web.dto.User.User;
 import io.maksym.web.actions.SimpleAction;
 import io.maksym.web.util.DataGenerators;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 
-import static io.maksym.web.validation.ValidationConstants.*;
-import static io.maksym.web.validation.ValidationConstants.NAME_MAX_LENGTH;
+import static io.maksym.web.util.Constants.*;
 
-public abstract class BaseTest {
+public class BaseTest implements SimpleAction {
     protected String email = "<EMAIL>";
     protected String password = "<PASSWORD>";
+    protected String token = "<TOKEN>";
+
     @BeforeEach
     public void setup(){
         RestAssured.baseURI = ApiEndpoints.BASE_URL;
-
-        SimpleAction simpleAction = new SimpleAction();
         String passwordCred = new DataGenerators().generateRandomPassword(PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH);
-        var createdUser = simpleAction.createUser(new User(new DataGenerators().generateRandomName(NAME_MIN_LENGTH, NAME_MAX_LENGTH), new DataGenerators().generateRandomEmail(true), passwordCred));
+        RegistrationSuccessfulResponse createdUser = createUser(new User(new DataGenerators().generateRandomName(NAME_MIN_LENGTH, NAME_MAX_LENGTH), new DataGenerators().generateRandomEmail(true), passwordCred));
         email = createdUser.getData().getEmail();
         password = passwordCred;
+
+        LoginResponse loggedInUser = logIn(email, password);
+        token = loggedInUser.getData().getToken();
+        System.out.println("Token: " + token);
     }
 
 }
