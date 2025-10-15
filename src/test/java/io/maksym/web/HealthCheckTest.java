78400d95fd3eac4a7da80a20c48af9ae74eb05a4
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-15 23:19:01 +0200

    Separate tests

--- src/test/java/io/maksym/web/HealthCheckTest.java
+++ src/test/java/io/maksym/web/HealthCheckTest.java
@@ -1,4 +1,29 @@
 package io.maksym.web;
 
-public class HealthCheckTest {
+import io.maksym.web.base.BaseTest;
+import io.maksym.web.dto.HealthCheck.HealthCheckResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static io.maksym.web.enums.ErrorMessage.HEALTH_CHECK_MESSAGE;
+import static io.maksym.web.enums.StatusCode.SUCCESSFUL_STATUS;
+import static io.maksym.web.util.Constants.EXPECTED_SUCCESS;
+import static io.maksym.web.util.Constants.REPEAT_COUNT;
+
+public class HealthCheckTest extends BaseTest {
+    @RepeatedTest(value = REPEAT_COUNT, name = "{displayName} : {currentRepetition}/{totalRepetitions}")
+    @DisplayName("Verify Successfull Health Check")
+
+    void verifySuccessfullHealthCheck() {
+
+        HealthCheckResponse response = checkHealth();
+        System.out.println(response);
+
+        Assertions.assertAll("Health Check Response",
+                ()->Assertions.assertEquals(SUCCESSFUL_STATUS.getStatus(), response.getStatus(), "Incorrect status code"),
+                ()->Assertions.assertEquals(HEALTH_CHECK_MESSAGE.getMessage(), response.getMessage(), "Incorrect message"),
+                ()->Assertions.assertEquals(EXPECTED_SUCCESS, response.isSuccess(), "Incorrect success status")
+        );
+    }
 }
