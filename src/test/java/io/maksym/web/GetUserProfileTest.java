8c688935eb22071a510684747867eb9e7f5e9dcf
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-23 21:43:00 +0200

    add tests for Profile data

--- src/test/java/io/maksym/web/GetUserProfileTest.java
+++ src/test/java/io/maksym/web/GetUserProfileTest.java
@@ -3,6 +3,7 @@ package io.maksym.web;
 import io.maksym.web.base.BaseTest;
 import io.maksym.web.dto.Profile.ProfileResponse;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 
 import static io.maksym.web.enums.ErrorMessage.PROFILE_SUCCESSFUL;
@@ -16,25 +17,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GetUserProfileTest extends BaseTest {
 
     @RepeatedTest(value = REPEAT_COUNT, name = "{displayName} : {currentRepetition}/{totalRepetitions}")
+    @DisplayName("Verify that user is able to fetch [Profile] data")
     public void getUserProfileTest(){
         ProfileResponse response = getUserProfile(token);
         assertEquals(SUCCESSFUL_STATUS.getStatus(), response.getStatus(), "Incorrect status code");
-        assertEquals(EXPECTED_SUCCESS_TRUE, response.getSuccess(), "Incorrect success status");
+        assertEquals(EXPECTED_SUCCESS_TRUE, response.isSuccess(), "Incorrect success status");
         assertEquals(PROFILE_SUCCESSFUL.getMessage(), response.getMessage());
 
-        Assertions.assertAll("User Profile Response",
-                () -> assertEquals(email, response.getData().getEmail(), "Incorrect email"),
-                () -> assertEquals(name, response.getData().getName(), "Incorrect name"),
-                () -> assertEquals(id, response.getData().getId(), "Incorrect id")
+        Assertions.assertAll("Verify that user is able to fetch [Profile] data",
+                () -> assertEquals(email, response.getData().getEmail(), "Incorrect [Email]"),
+                () -> assertEquals(name, response.getData().getName(), "Incorrect [Name]"),
+                () -> assertEquals(id, response.getData().getId(), "Incorrect [Id]")
         );
     }
 
     @RepeatedTest(value = REPEAT_COUNT, name = "{displayName} : {currentRepetition}/{totalRepetitions}")
+    @DisplayName("Verify that user is NOT able to fetch [Profile] data with Invalid Token")
     public void getUserProfileWithWrongTokenTest(){
         ProfileResponse response = getUserProfile("wrongToken");
-        assertAll("Unauthorized Profile Response",
+        assertAll("Verify that user is NOT able to fetch [Profile] data with Invalid Token",
                 () -> assertEquals(UNAUTHORIZED_STATUS.getStatus(), response.getStatus(), "Incorrect status code"),
-                () -> assertEquals(EXPECTED_SUCCESS_FALSE, response.getSuccess(), "Incorrect success status"),
+                () -> assertEquals(EXPECTED_SUCCESS_FALSE, response.isSuccess(), "Incorrect success status"),
                 () -> assertEquals(UNAUTHORIZED_MESSAGE.getMessage(), response.getMessage(), "Incorrect message")
         );
     }
