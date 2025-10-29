4362267eb9c6096926de6aee74c2a8bbedcb0ed7
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-29 19:28:38 +0100

    add SchemaResponseValidator to handle response schema validation

--- src/main/java/io/maksym/web/util/SchemaResponseValidator.java
+++ src/main/java/io/maksym/web/util/SchemaResponseValidator.java
@@ -1,4 +1,38 @@
 package io.maksym.web.util;
 
+import io.restassured.response.Response;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class SchemaResponseValidator {
+
+    private static final Logger logger = LoggerFactory.getLogger(SchemaResponseValidator.class);
+
+    public static boolean assertResponseSchema(String pathToSchema, Response response) {
+        boolean result = false;
+        try {
+            if (response.getBody().asString().equals("[]")) {
+                logger.info("Response for - "
+                        + pathToSchema
+                        + "has an empty array. Skipping schema validation.");
+            } else {
+                response
+                        .then()
+                        .assertThat()
+                        .body(JsonSchemaValidator
+                                .matchesJsonSchemaInClasspath(pathToSchema));
+                logger.info("Schema validation - " + pathToSchema + " - PASSED");
+                result = true;
+            }
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (!result) {
+                logger.info("Schema validation - " + pathToSchema + " - FAILED");
+            }
+        }
+    }
 }
