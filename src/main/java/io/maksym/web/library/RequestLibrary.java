01e96d78dac4b49493a3e9078a7c703dfe0d0c28
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-29 19:28:38 +0100

    refactor RequestLibrary

--- src/main/java/io/maksym/web/library/RequestLibrary.java
+++ src/main/java/io/maksym/web/library/RequestLibrary.java
@@ -1,113 +1,69 @@
 package io.maksym.web.library;
 
-import io.maksym.web.dto.HealthCheck.HealthCheckResponse;
-import io.maksym.web.dto.Login.LoginResponse;
-import io.maksym.web.dto.Profile.ProfileResponse;
-import io.maksym.web.dto.Registration.RegistrationSuccResponse.RegistrationSuccessfulResponse;
-import io.maksym.web.dto.User.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-
-import java.util.HashMap;
-
-import static io.maksym.web.config.ApiEndpoints.*;
 import static io.maksym.web.config.ApiEndpoints.BASE_URL;
-import static io.maksym.web.config.ApiEndpoints.ENDPOINT_LOG_IN;
 import static io.restassured.RestAssured.given;
 
-public interface RequestLibrary {
+public class RequestLibrary {
     private static RequestSpecification getRequestSpec() {
         return given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON);
     }
 
-    default HealthCheckResponse getCheckHealth() {
+    public Response getCheckH(String url) {
         return getRequestSpec()
                 .when()
-                .get(BASE_URL + ENDPOINT_HEALTH_CHECK).then().log().all().extract().as(HealthCheckResponse.class);
+                .get(BASE_URL + url).then().log().all().extract().response();
     }
-
-    default RegistrationSuccessfulResponse postCreateUser(User user){
+    public Response postReq(String url, Record record){
         return getRequestSpec()
-                .body(user)
+                .body(record)
                 .when()
-                .post(BASE_URL + ENDPOINT_CREATE_USER).then().log().all().extract().as(RegistrationSuccessfulResponse.class);
+                .post(BASE_URL + url).then().log().all().extract().response();
     }
-
-    default LoginResponse postLogIn(String email, String password){
-        return getRequestSpec()
-                .body(new HashMap<String, String>() {{ put("email", email); put("password", password);}} )
-                .when()
-                .post(BASE_URL + ENDPOINT_LOG_IN).then().log().all().extract().as(LoginResponse.class);
-    }
-    default ProfileResponse getUserProfile(String token){
+    public Response getReq(String url, String token){
         return given()
                 .header("accept", "application/json")
                 .header("x-auth-token", token)
                 .when()
-                .get(BASE_URL + ENDPOINT_GET_USER_PROFILE)
+                .get(BASE_URL + url)
                 .then()
                 .log().all()
-                .extract().as(ProfileResponse.class);
+                .extract().response();
     }
-    default ProfileResponse updateUserProfileName(String token, String name){
+    public Response patchReq(String url, String token, Record record){
+        String jsonBody = null;
+        try{
+            jsonBody = new ObjectMapper().writeValueAsString(record);
+            System.out.println(jsonBody);
+
+        }catch(JsonProcessingException e){
+            System.out.println("Error while converting object to json");
+            e.printStackTrace();
+        }
+
         return given()
                 .header("accept", "application/json")
                 .header("Content-Type", "application/json")
                 .header("x-auth-token", token)
-                .body(new HashMap<String, String>() {{ put("name", name); }})
+                .body(jsonBody)
                 .when()
-                .patch(BASE_URL + ENDPOINT_UPDATE_USER_PROFILE)
+                .patch(BASE_URL + url)
                 .then()
                 .log().all()
-                .extract().as(ProfileResponse.class);
+                .extract().response();
 
     }
-    default ProfileResponse updateUserProfileCompany(String token,  String name, String company){
-        return given()
-                .header("accept", "application/json")
-                .header("Content-Type", "application/json")
-                .header("x-auth-token", token)
-                .body(new HashMap<String, String>() {{ put("name", name); put("company", company); }})
-                .when()
-                .patch(BASE_URL + ENDPOINT_UPDATE_USER_PROFILE)
-                .then()
-                .log().all()
-                .extract().as(ProfileResponse.class);
-
-    }
-    default ProfileResponse updateUserProfilePhone(String token, String name, String phone){
-        return given()
-                .header("accept", "application/json")
-                .header("Content-Type", "application/json")
-                .header("x-auth-token", token)
-                .body(new HashMap<String, String>() {{ put("phone", phone); put("name", name);}})
-                .when()
-                .patch(BASE_URL + ENDPOINT_UPDATE_USER_PROFILE)
-                .then()
-                .log().all()
-                .extract().as(ProfileResponse.class);
-    }
-    default ProfileResponse updateUserProfile(String token, String name, String phone, String company){
-        return given()
-                .header("accept", "application/json")
-                .header("Content-Type", "application/json")
-                .header("x-auth-token", token)
-                .body(new HashMap<String, String>() {{ put("name", name); put("company", company); put("phone", phone);}})
-                .when()
-                .patch(BASE_URL + ENDPOINT_UPDATE_USER_PROFILE)
-                .then()
-                .log().all()
-                .extract().as(ProfileResponse.class);
-    }
-    default HealthCheckResponse deleteUser(String token){
+    public Response deleteReq(String url, String token){
         return getRequestSpec()
                 .header("x-auth-token", token)
                 .when()
-                .delete(ENDPOINT_DELETE_USER_PROFILE)
-                .then().log().all().extract().as(HealthCheckResponse.class);
-
+                .delete(BASE_URL + url)
+                .then().log().all().extract().response();
     }
-
 }
