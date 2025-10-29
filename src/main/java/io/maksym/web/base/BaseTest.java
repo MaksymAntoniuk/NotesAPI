e4660feee9566580e3c01d3e22dbe602ce984b2f
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-10-29 19:28:37 +0100

    extend BaseTest class with SimpleAction

--- src/main/java/io/maksym/web/base/BaseTest.java
+++ src/main/java/io/maksym/web/base/BaseTest.java
@@ -1,18 +1,21 @@
 package io.maksym.web.base;
 
+import io.maksym.web.Records.LoginBody;
+import io.maksym.web.Records.UserBody;
+import io.maksym.web.actions.SimpleAction;
 import io.maksym.web.config.ApiEndpoints;
 import io.maksym.web.dto.Login.LoginResponse;
 import io.maksym.web.dto.Registration.RegistrationSuccResponse.RegistrationSuccessfulResponse;
-import io.maksym.web.dto.User.User;
-import io.maksym.web.actions.SimpleAction;
 import io.maksym.web.util.DataGenerators;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import static io.maksym.web.config.ApiEndpoints.ENDPOINT_CREATE_USER;
+import static io.maksym.web.config.ApiEndpoints.ENDPOINT_LOG_IN;
 import static io.maksym.web.util.Constants.*;
 
-public class BaseTest implements SimpleAction {
+public class BaseTest extends SimpleAction {
     protected String email = "<EMAIL>";
     protected String password = "<PASSWORD>";
     protected String token = "<TOKEN>";
@@ -23,12 +26,12 @@ public class BaseTest implements SimpleAction {
     public void setup(){
         RestAssured.baseURI = ApiEndpoints.BASE_URL;
         String passwordCred = new DataGenerators().generateRandomPassword(PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH);
-        RegistrationSuccessfulResponse createdUser = createUser(new User(new DataGenerators().generateRandomName(NAME_MIN_LENGTH, NAME_MAX_LENGTH), new DataGenerators().generateRandomEmail(true), passwordCred));
+        RegistrationSuccessfulResponse createdUser = postRequest(ENDPOINT_CREATE_USER, new UserBody(new DataGenerators().generateRandomName(NAME_MIN_LENGTH, NAME_MAX_LENGTH), new DataGenerators().generateRandomEmail(true), passwordCred)).as(RegistrationSuccessfulResponse.class);
 
         email = createdUser.getData().getEmail();
         password = passwordCred;
 
-        LoginResponse loggedInUser = logIn(email, password);
+        LoginResponse loggedInUser = postRequest( ENDPOINT_LOG_IN, new LoginBody(email, password)).as(LoginResponse.class);
 
         token = loggedInUser.getData().getToken();
         id = loggedInUser.getData().getId();
