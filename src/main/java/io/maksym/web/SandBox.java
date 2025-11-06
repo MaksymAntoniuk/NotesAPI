4ff9152322e8c192f0e7d20593644ebf1e862d78
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-11-06 22:17:32 +0100

    refactor pom.xml: update dependencies and project configuration

--- src/main/java/io/maksym/web/SandBox.java
+++
@@ -1,9 +0,0 @@
-package io.maksym.web;
-
-import static io.maksym.web.enums.StatusCode.CREATED_STATUS;
-
-public class SandBox {
-    public static void main(String[] args) {
-        System.out.println(CREATED_STATUS);
-    }
-}
---
+++ src/test/java/io/maksym/web/UpdateUserProfileTest.java
@@ -0,0 +1,4 @@
+package io.maksym.web;
+
+public class UpdateUserProfileTest {
+}
