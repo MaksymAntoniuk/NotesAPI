59d31b59a51bd30b092ea889c7ed36411354e2a6
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-11-06 22:17:33 +0100

    add refactoring

--- src/test/java/io/maksym/web/HealthCheckTest.java
+++ src/test/java/io/maksym/web/HealthCheckTest.java
@@ -2,6 +2,7 @@ package io.maksym.web;
 
 import io.maksym.web.base.BaseTest;
 import io.maksym.web.dto.HealthCheck.HealthCheckResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
@@ -12,18 +13,21 @@ import static io.maksym.web.enums.StatusCode.SUCCESSFUL_STATUS;
 import static io.maksym.web.util.Constants.EXPECTED_SUCCESS_TRUE;
 import static io.maksym.web.util.Constants.REPEAT_COUNT;
 import static io.maksym.web.util.SchemaResponseValidator.assertResponseSchema;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HealthCheckTest extends BaseTest {
     @RepeatedTest(value = REPEAT_COUNT, name = "{displayName} : {currentRepetition}/{totalRepetitions}")
     @DisplayName("Verify Successfull Health Check")
     void verifySuccessfullHealthCheck() {
-        HealthCheckResponse response = getCheckHealth(ENDPOINT_HEALTH_CHECK).as(HealthCheckResponse.class);
-        boolean validationSchema = assertResponseSchema("healthcheck-schema.json", getCheckHealth(ENDPOINT_HEALTH_CHECK));
-        Assertions.assertAll("Verify Successfull Health Check",
-                ()->Assertions.assertTrue(validationSchema, "Incorrect response schema"),
-                ()->Assertions.assertEquals(SUCCESSFUL_STATUS.getStatus(), response.getStatus(), "Incorrect status code"),
-                ()->Assertions.assertEquals(HEALTH_CHECK_MESSAGE.getMessage(), response.getMessage(), "Incorrect message"),
-                ()->Assertions.assertEquals(EXPECTED_SUCCESS_TRUE, response.isSuccess(), "Incorrect success status")
+        Response responseValidationSchema = getCheckHealth(ENDPOINT_HEALTH_CHECK);
+        boolean validationSchema = assertResponseSchema("healthcheck-schema.json", responseValidationSchema);
+        HealthCheckResponse response = responseValidationSchema.as(HealthCheckResponse.class);
+
+        assertAll("Verify Successfull Health Check",
+                ()->assertTrue(validationSchema, "Incorrect response schema"),
+                ()->assertEquals(SUCCESSFUL_STATUS.getStatus(), response.getStatus(), "Incorrect status code"),
+                ()->assertEquals(HEALTH_CHECK_MESSAGE.getMessage(), response.getMessage(), "Incorrect message"),
+                ()->assertEquals(EXPECTED_SUCCESS_TRUE, response.isSuccess(), "Incorrect success status")
         );
 
     }
