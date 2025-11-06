71b966caee62591c8b29001d8ee0f4ef9701ab5a
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-11-06 22:17:32 +0100

    change from class to interface

--- src/main/java/io/maksym/web/actions/SimpleAction.java
+++ src/main/java/io/maksym/web/actions/SimpleAction.java
@@ -1,21 +1,22 @@
 package io.maksym.web.actions;
-import io.maksym.web.library.RequestLibrary;
 import io.restassured.response.Response;
 
-public class SimpleAction extends RequestLibrary {
-    public Response getCheckHealth(String url){
+import static io.maksym.web.library.RequestLibrary.*;
+
+public  interface SimpleAction {
+    default Response getCheckHealth(String url){
         return getCheckH(url);
     };
-    public Response postRequest(String url, Record record){
+    default Response postRequest(String url, Record record){
         return postReq(url, record);
     };
-    public Response getRequest(String url, String token){
+    default Response getRequest(String url, String token){
         return getReq(url, token);
     };
-    public Response patchRequest(String url, String token, Record record){
+    default Response patchRequest(String url, String token, Record record){
         return patchReq(url, token, record);
     };
-    public Response deleteRequest(String url, String token){
+    default Response deleteRequest(String url, String token){
         return deleteReq(url, token);
     };
 }
