9d96d4003387550645e2fbf07b966612f87e723b
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-11-12 13:08:58 +0100

    add new methods generated random data for Note

--- src/main/java/io/maksym/web/util/DataGenerators.java
+++ src/main/java/io/maksym/web/util/DataGenerators.java
@@ -1,6 +1,11 @@
 package io.maksym.web.util;
 
 import com.github.javafaker.Faker;
+import io.maksym.web.enums.CategoryNote;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DataGenerators {
     String fakeName;
@@ -20,7 +25,9 @@ public class DataGenerators {
        if (!isValid){
            return "invalidEmail";
        }
-           return new Faker().artist().name().replaceAll("\\s+", "_") + System.currentTimeMillis() + "@gmail.com";
+       String email = new Faker().artist().name().replaceAll("\\s+", "_") +
+               System.currentTimeMillis() + "@gmail.com";
+           return email.toLowerCase();
     }
 
     public String generateRandomPassword(int minLength, int maxLength) {
@@ -31,6 +38,34 @@ public class DataGenerators {
         return new Faker().phoneNumber().subscriberNumber(10);
     }
     public String generateRandomCompany() {
-        return new Faker().company().name();
+        String compannyName = new Faker().company().name();
+        if (compannyName.length() > 30){
+            return compannyName.substring(0, 30);
+        }
+        return compannyName;
+    }
+
+    public String generateRandomTitle() {
+        String title = new Faker().book().title();
+        if (title.length() > 100){
+            return title.substring(0, 100);
+        }
+        return title;
+    }
+
+    public String generateRandomDescription() {
+        String description = new Faker().lorem().paragraph();
+        if (description.length() > 1000){
+            return description.substring(0, 1000);
+        }
+        return description;
+    }
+
+    public String generateRandomCategoryNote(){
+        List<String> categories = new ArrayList<String>(List.of(
+                CategoryNote.HOME.getCategory(),
+                CategoryNote.PERSONAL.getCategory(),
+                CategoryNote.WORK.getCategory()));
+        return categories.get(new Faker().number().numberBetween(0, categories.size() - 1));
     }
 }
