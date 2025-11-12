d4f51837af18d79334d1f1d0683c713218e633e5
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-11-12 13:04:20 +0100

    add new request methods to work with CRUD Note

--- src/main/java/io/maksym/web/requests/actions/SimpleAction.java
+++ src/main/java/io/maksym/web/requests/actions/SimpleAction.java
@@ -1,8 +1,8 @@
-package io.maksym.web.actions;
+package io.maksym.web.requests.actions;
 import io.restassured.response.Response;
 
 import static io.maksym.web.config.ApiEndpoints.*;
-import static io.maksym.web.library.RequestLibrary.*;
+import static io.maksym.web.requests.library.RequestLibrary.*;
 
 public  interface SimpleAction {
     default Response checkHealthOfApiService(){
@@ -26,4 +26,20 @@ public  interface SimpleAction {
     static Response createNote(String token, Record record){
         return postRequest(ENDPOINT_CREATE_NOTE, token, record);
     }
+    default Response getAllNotes(String token){
+        return getRequest(ENDPOINT_GET_ALL_NOTES, token);
+    }
+    default Response getNoteById(String token, String id){
+        return getRequest(ENDPOINT_GET_NOTE_BY_ID + id, token);
+    }
+    default Response deleteNoteById(String token, String id){
+        return deleteRequest(ENDPOINT_DELETE_NOTE + id, token);
+    }
+    default Response updateCompletedStatus(String token, String id, Record record){
+        return patchRequest(ENDPOINT_UPDATE_COMPLETED_STATUS + id, token, record);
+    };
+    default Response updateNote(String token, String id, Record record){
+        return putRequest(ENDPOINT_UPDATE_NOTE + id, token, record);
+    };
+
 }
