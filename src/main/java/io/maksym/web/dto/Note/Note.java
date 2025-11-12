37e189b33ca4a44379272fbb569e22f6c9df6dd6
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-11-12 12:59:44 +0100

    add new DTOs for Note response

--- src/main/java/io/maksym/web/dto/Note/Note.java
+++ src/main/java/io/maksym/web/dto/Note/Note.java
@@ -1,4 +1,11 @@
-package io.maksym.web.dto;
+package io.maksym.web.dto.Note;
 
+import lombok.Data;
+
+@Data
 public class Note {
+    private boolean success;
+    private int status;
+    private String message;
+    private NoteResponse data;
 }
--- src/main/java/io/maksym/web/dto/Note/NoteResponse.java
+++ src/main/java/io/maksym/web/dto/Note/NoteResponse.java
@@ -1,4 +1,17 @@
 package io.maksym.web.dto.Note;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
 public class NoteResponse {
+    private String id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private String created_at;
+    private String updated_at;
+    private String category;
+    @JsonProperty("user_id")
+    private String userId;
 }
