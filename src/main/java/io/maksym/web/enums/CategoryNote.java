b9406d4181e965d95bc4bc375540b18f812e8717
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2025-11-12 13:00:38 +0100

    add enum with Note category

--- src/main/java/io/maksym/web/enums/CategoryNote.java
+++ src/main/java/io/maksym/web/enums/CategoryNote.java
@@ -1,4 +1,17 @@
 package io.maksym.web.enums;
 
 public enum CategoryNote {
+    HOME("Home"),
+    WORK("Work"),
+    PERSONAL("Personal");
+
+    private final String category;
+
+    CategoryNote(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
