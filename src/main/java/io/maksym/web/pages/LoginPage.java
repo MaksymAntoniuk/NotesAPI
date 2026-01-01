660910daaf7d8641ba5d8e34c77a1d40037f87e2
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2026-01-01 21:57:28 +0100

    add class for Login Page

---
+++ src/main/java/io/maksym/web/pages/LoginPage.java
@@ -0,0 +1,93 @@
+package io.maksym.web.pages;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import io.maksym.web.components.FlashAlert;
+import io.maksym.web.records.ui.UiUserLogIn;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class LoginPage extends BasePage{
+    private final FlashAlert flashAlert;
+    private final Locator username = page.locator("//input[@name='username']");
+    private final Locator password = page.locator("//input[@name='password']");
+    private final Locator hereLink = page.locator("//a[@href='/register']");
+    private final Locator homeLink = page.getByRole(
+            AriaRole.LINK,
+            new Page.GetByRoleOptions().setName("Home")
+    );
+
+    private final Locator submit = page.getByRole(AriaRole.BUTTON,
+            new Page.GetByRoleOptions().setName("Login"));
+
+
+    public LoginPage(Page page) {
+        super(page);
+        this.flashAlert = new FlashAlert(page);
+    }
+
+    @Override
+    protected String path() {
+        return "/login";
+    }
+
+    public void fillUserName(String username){
+        this.username.fill(username);
+    }
+
+    public void fillPassword(String password){
+        this.password.fill(password);
+    }
+
+    public void loginClick(){
+        submit.click();
+    }
+
+    public FlashAlert flashAlert(){
+        return flashAlert;
+    }
+
+    public SecurePage loginAs(UiUserLogIn user){
+        fillUserName(user.username());
+        fillPassword(user.password());
+        loginClick();
+        page.waitForURL("**/secure");
+        return new SecurePage(page);
+    }
+
+    public LoginPage loginWithInvalidUser(UiUserLogIn user){
+        fillUserName(user.username());
+        fillPassword(user.password());
+        loginClick();
+        return new LoginPage(page);
+    }
+
+    public RegisterPage navigateToRegisterPage(){
+        hereLink.click();
+        return new RegisterPage(page);
+    }
+
+    public LoginPage loginPageShouldBeOpened(){
+        assertThat(page).hasURL("**/login");
+        assertThat(
+                page.getByRole(
+                        AriaRole.HEADING,
+                        new Page.GetByRoleOptions().setLevel(1)
+                )
+        ).containsText("Test Login page");
+
+        return this;
+    }
+    public void homeLinkShouldBeVisible(){
+        assertThat(homeLink).isVisible();
+    }
+    public HomePage navigateToHomePage(){
+        homeLink.click();
+        return new HomePage(page);
+    }
+
+    public void isHereLinkIsVisible(){
+        assertThat(hereLink).isVisible();
+    }
+
+}
