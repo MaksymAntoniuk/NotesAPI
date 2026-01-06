2b485af8b2843e2fa9c4884e9502ae8d3883afcb
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2026-01-06 20:02:19 +0100

    update BaseTest class to run in CI

--- src/test/java/io/maksym/web/UI/BaseTest.java
+++ src/test/java/io/maksym/web/UI/BaseTest.java
@@ -1,55 +1,41 @@
-package io.maksym.web.ui;
+package io.maksym.web.UI;
 
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.nio.file.Paths;
 import static io.maksym.web.config.ApiEndpoints.BASE_URL_UI;
 
 public abstract class BaseTest {
 
-    private static final ThreadLocal<Playwright> threadPlaywright = new ThreadLocal<>();
-    private static final ThreadLocal<Browser> threadBrowser = new ThreadLocal<>();
-
+    protected Playwright playwright;
+    protected Browser browser;
     protected BrowserContext context;
     protected Page page;
 
     @BeforeEach
-    public void beforeEach() {
-        if (threadPlaywright.get() == null) {
-            Playwright pw = Playwright.create();
-            String isHeadlessString = System.getenv("HEADLESS");
-            boolean isHeadless = isHeadlessString != null && Boolean.parseBoolean(isHeadlessString);
+    void setUp() {
+        boolean headless = Boolean.parseBoolean(
+                System.getenv().getOrDefault("HEADLESS", "true")
+        );
 
-            Browser br = pw.chromium().launch(
-                    new BrowserType.LaunchOptions().setHeadless(isHeadless)
-            );
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions().setHeadless(headless)
+        );
 
-            threadPlaywright.set(pw);
-            threadBrowser.set(br);
-        }
-
-        context = threadBrowser.get().newContext(new Browser.NewContextOptions()
-                .setViewportSize(1920, 1080)
-                .setRecordVideoDir(Paths.get("./target/video")));
+        context = browser.newContext(
+                new Browser.NewContextOptions().setViewportSize(1920, 1080)
+        );
 
         page = context.newPage();
-        navigateToPageUrl(BASE_URL_UI);
+        page.navigate(BASE_URL_UI);
     }
 
     @AfterEach
-    public void afterEach() {
-        if (context != null) {
-            context.close();
-        }
-    }
-
-    public void navigateToPageUrl(String url) {
-        page.navigate(url);
-    }
-
-    public void linkClick(String linkTitle) {
-        page.locator(linkTitle).click();
+    void tearDown() {
+        context.close();
+        browser.close();
+        playwright.close();
     }
 }
