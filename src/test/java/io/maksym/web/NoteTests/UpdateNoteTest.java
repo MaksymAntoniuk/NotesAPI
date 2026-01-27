95f6cfefa7fc2506ca928edc1a454eb230d4744f
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2026-01-27 21:26:51 +0100

    add a method to gather created Notes for test teardown

--- src/test/java/io/maksym/web/NoteTests/UpdateNoteTest.java
+++ src/test/java/io/maksym/web/NoteTests/UpdateNoteTest.java
@@ -56,6 +56,7 @@ public class UpdateNoteTest extends BaseTest {
                 () -> assertEquals(description, response.getData().getDescription(),"Invalid Description"),
                 () -> assertEquals(category, response.getData().getCategory(), "Invalid Category"),
                 () -> assertTrue(response.getData().isCompleted(), "Invalid [Completed] status"));
+        registerCreatedNote(noteId);
     }
 
     @DisplayName("Verify that user is able to update [Note] fields")
@@ -95,6 +96,6 @@ public class UpdateNoteTest extends BaseTest {
                 () -> assertEquals(newCategory, response.getData().getCategory(), "Invalid Category"),
                 () -> assertTrue(response.getData().isCompleted(), "Invalid [Completed] status"));
 
-        deleteNoteById(token, noteId);
+        registerCreatedNote(noteId);
     }
 }
