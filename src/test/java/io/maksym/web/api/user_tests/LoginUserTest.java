a947c806562842cf2306b96b724963fd9ccbf697
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2026-02-20 16:18:31 +0100

    update import statements in LoginUserTest and LoginTest

--- src/test/java/io/maksym/web/api/user_tests/LoginUserTest.java
+++ src/test/java/io/maksym/web/api/user_tests/LoginUserTest.java
@@ -4,7 +4,7 @@ import io.maksym.web.records.LoginBody;
 import io.maksym.web.records.UserBody;
 import io.maksym.web.api.base.BaseTest;
 import io.maksym.web.dto.Login.LoginResponse;
-import io.maksym.web.dto.Registration.RegistrationSuccResponse.RegistrationSuccessfulResponse;
+import io.maksym.web.dto.Registration.RegistrationSuccessfulResponse;
 import io.maksym.web.requests.actions.SimpleAction;
 import io.maksym.web.util.DataGenerators;
 import io.qameta.allure.Description;
--- src/test/java/io/maksym/web/ui/practice_tests/LoginTest.java
+++ src/test/java/io/maksym/web/ui/practice_tests/LoginTest.java
@@ -1,10 +1,11 @@
 package io.maksym.web.ui.practice_tests;
 
 import io.maksym.web.pages.*;
-import io.maksym.web.pages.HomePage;
-import io.maksym.web.pages.LoginPage;
+
 import io.maksym.web.pages.RegisterPage;
 import io.maksym.web.pages.SecurePage;
+import io.maksym.web.pages.practice.HomePage;
+import io.maksym.web.pages.practice.LoginPage;
 import io.maksym.web.records.ui.UiUserLogIn;
 import io.maksym.web.ui.pages.BaseTest;
 import io.maksym.web.util.DataGenerators;
