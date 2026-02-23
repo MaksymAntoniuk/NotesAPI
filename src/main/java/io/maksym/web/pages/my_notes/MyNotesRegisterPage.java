af336470c860c9951f13811982ca106a58b9cc3d
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2026-02-23 23:03:04 +0100

    update `MyNotesRegisterPage` to handle registration API response and return user ID

--- src/main/java/io/maksym/web/pages/my_notes/MyNotesRegisterPage.java
+++ src/main/java/io/maksym/web/pages/my_notes/MyNotesRegisterPage.java
@@ -1,7 +1,10 @@
 package io.maksym.web.pages.my_notes;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Response;
 import com.microsoft.playwright.options.AriaRole;
 import io.maksym.web.components.my_notes.AlertToast;
 import io.maksym.web.components.my_notes.SuccessRegistration;
@@ -38,16 +41,19 @@ public class MyNotesRegisterPage extends BasePage {
         emailInput.waitFor();
         emailInput.fill(email);
     }
+
     @Step("Enter name")
     public void fillName(String name){
         nameInput.waitFor();
         nameInput.fill(name);
     }
+
     @Step("Enter password")
     public void fillPassword(String password){
         passwordInput.waitFor();
         passwordInput.fill(password);
     }
+
     @Step("Enter confirm password")
     public void fillConfirmPassword(String confirmPassword){
         confirmPasswordInput.waitFor();
@@ -55,18 +61,35 @@ public class MyNotesRegisterPage extends BasePage {
         page.keyboard().type(confirmPassword);
         confirmPasswordInput.fill(confirmPassword);
     }
+
     @Step("Click on register button")
     public MyNotesLoginPage clickRegisterBtn(){
         registerBtn.click();
         return new MyNotesLoginPage(page);
     }
+
     @Step("Submit registration form")
-    public MyNotesLoginPage registerNewUser(MyNoteRegisterUser user){
+    public String registerNewUser(MyNoteRegisterUser user){
         fillEmail(user.email());
         fillName(user.username());
         fillPassword(user.password());
         fillConfirmPassword(user.password());
-        return clickRegisterBtn();
+
+        com.microsoft.playwright.Response response = page.waitForResponse(
+                res -> res.url().contains("https://practice.expandtesting.com/notes/api/users/register") && res.request().method().equals("POST"),
+                () -> {
+                    clickRegisterBtn();
+                }
+        );
+
+        String body = response.text();
+        JsonObject json = new Gson().fromJson(body, JsonObject.class);
+
+        if(response.status() == 201 && json.getAsJsonObject("data").get("id") != null){
+            return json.getAsJsonObject("data").get("id").getAsString();
+        }
+
+        return null;
     }
 
     @Step("Click on link to login page")
@@ -74,15 +97,18 @@ public class MyNotesRegisterPage extends BasePage {
         successRegistration.goToLoginPage();
         return new MyNotesLoginPage(page);
     }
+
     @Step("Click on register with Google button")
     public void clickRegisterGoogleBtn(){
         registerGoogleBtn.click();
     }
+
     @Step("Click on register with LinkedIn button")
     public void clickRegisterLinkedInBtn(){
         registerLinkedInBtn.click();
     }
 
+    @Step("Assert Registration page is opened")
     public MyNotesRegisterPage assertRegistrationPageIsOpened(){
         registerPageTitle.waitFor();
         assert(registerPageTitle).isVisible();
