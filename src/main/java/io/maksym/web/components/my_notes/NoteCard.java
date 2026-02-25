78751aa5d59b01651be42e292a9b5cc7cf2a1dde
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2026-02-25 16:41:52 +0100

    update `NoteCard` with category color and timestamp assertions

--- src/main/java/io/maksym/web/components/my_notes/NoteCard.java
+++ src/main/java/io/maksym/web/components/my_notes/NoteCard.java
@@ -2,8 +2,15 @@ package io.maksym.web.components.my_notes;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.options.AriaRole;
+import io.maksym.web.enums.CategoryNote;
+import io.maksym.web.pages.my_notes.MyNotesViewPage;
 import io.maksym.web.records.ui.MyNoteBody;
 import io.qameta.allure.Step;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 
@@ -25,7 +32,7 @@ public class NoteCard {
         noteDescription = root.getByTestId("note-card-description");
         noteTimestamp = root.getByTestId("note-card-updated-at");
         completedCheckbox = root.getByTestId("toggle-note-switch");
-        viewNoteBtn = root.getByRole(AriaRole.BUTTON, new Locator.GetByRoleOptions().setName("View"));
+        viewNoteBtn = root.getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName("View"));
         noteEditBtn = root.getByRole(AriaRole.BUTTON, new Locator.GetByRoleOptions().setName("Edit"));
         noteDeleteBtn = root.getByRole(AriaRole.BUTTON, new Locator.GetByRoleOptions().setName("Delete"));
     }
@@ -34,21 +41,57 @@ public class NoteCard {
     public void assertNoteData(MyNoteBody note){
         assertThat(this.noteTitle).hasText(note.title());
         assertThat(this.noteDescription).hasText(note.description());
+        assertTimestampIsUpdated();
         if(note.completed()){
             assertThat(this.completedCheckbox).isChecked();
+            assertThat(noteTitle).hasCSS("background-color", "rgba(40, 46, 41, 0.6)");
+
         } else {
             assertThat(this.completedCheckbox).not().isChecked();
+            assertCategoryColor(note.category());
+        }
+    }
+
+    @Step("Assert color of category")
+    private void assertCategoryColor(CategoryNote category){
+        String expectedColor = switch (category) {
+            case HOME -> "rgb(255, 145, 0)";
+            case WORK -> "rgb(92, 107, 192)";
+            case PERSONAL -> "rgb(50, 140, 160)";
+        };
+        assertThat(this.noteTitle).hasCSS("background-color", expectedColor);
+    }
+
+    @Step("Assert timestamp")
+    public void assertTimestampIsUpdated(){
+        String uiTimeText = noteTimestamp.innerText();
+        System.out.println(uiTimeText);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy 'at' HH:mm:ss", Locale.ENGLISH);
+
+        LocalDateTime parsedUiTime = LocalDateTime.parse(uiTimeText, formatter);
+        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+
+        long secondsBetween = ChronoUnit.SECONDS.between(parsedUiTime, now);
+        System.out.println("Second Between: " + secondsBetween);
+
+        if (Math.abs(secondsBetween) > 60){
+            throw new AssertionError("Timestamp drift too high! UI (UTC): " + uiTimeText +
+                    ", System (UTC): " + now.format(formatter));
         }
     }
 
     @Step("Click on View button")
-    public void clickOnViewBtn(){
+    public MyNotesViewPage clickOnViewBtn(){
         viewNoteBtn.click();
+        return new MyNotesViewPage(root.page());
     }
+
     @Step("Click on Edit button")
     public void clickOnEditBtn(){
         noteEditBtn.click();
     }
+
     @Step("Click on Delete button")
     public void clickOnDeleteBtn(){
         noteDeleteBtn.click();
