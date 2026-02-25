021be843815926b17ef2820208d870074d82d588
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2026-02-25 16:43:21 +0100

    replace `AddNewNoteForm` with `NoteModal`

--- src/main/java/io/maksym/web/components/my_notes/NoteModal.java
+++ src/main/java/io/maksym/web/components/my_notes/NoteModal.java
@@ -6,9 +6,11 @@ import com.microsoft.playwright.options.AriaRole;
 import io.maksym.web.records.ui.MyNoteBody;
 import io.qameta.allure.Step;
 
+import java.util.regex.Pattern;
+
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
-public class AddNewNoteForm {
+public class NoteModal {
     Page page;
     private final Locator createBtn;
     private final Locator cancelBtn;
@@ -21,15 +23,15 @@ public class AddNewNoteForm {
 
     private final Locator title;
 
-    public AddNewNoteForm(Page page) {
+    public NoteModal(Page page) {
         this.page = page;
-        createBtn = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Create"));
+        createBtn = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName(Pattern.compile("Create|Save")));
         cancelBtn = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Cancel"));
         titleField = page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Title"));
         descriptionField = page.getByRole(AriaRole.TEXTBOX, new Page.GetByRoleOptions().setName("Description"));
         completedCheckbox = page.getByRole(AriaRole.CHECKBOX, new Page.GetByRoleOptions().setName("Completed"));
         categorySelect = page.getByTestId("note-category");
-        title = page.locator(".modal-title").getByText("Add New Note");
+        title = page.locator(".modal-title");
     }
 
     @Step("Assert Add new note form is open")
@@ -42,21 +44,36 @@ public class AddNewNoteForm {
     public void fillInTitleField(String title){
         titleField.fill(title);
     }
+
     @Step("Fill in description field")
     public void fillInDescriptionField(String description){
         descriptionField.fill(description);
     }
+
     @Step("Select {0} option")
     public void selectCategory(String category){
         categorySelect.selectOption(category);
     }
+
     @Step("Check completed checkbox")
     public void checkCompletedCheckbox(){
         completedCheckbox.check();
         assertThat(completedCheckbox).isChecked();
     }
+
     @Step("Create new note")
-    public void crateNewNote(MyNoteBody note) {
+    public void createNewNote(MyNoteBody note) {
+        fillInTitleField(note.title());
+        fillInDescriptionField(note.description());
+        selectCategory(note.category().getCategory());
+        if (note.completed()) {
+            checkCompletedCheckbox();
+        }
+        clickOnCreateBtn();
+    }
+
+    @Step("Edit note")
+    public void editNoteWithNewData(MyNoteBody note) {
         fillInTitleField(note.title());
         fillInDescriptionField(note.description());
         selectCategory(note.category().getCategory());
@@ -70,6 +87,7 @@ public class AddNewNoteForm {
     public void clickOnCreateBtn(){
         createBtn.click();
     }
+
     @Step("Click on Cancel button")
     public void clickOnCancelBtn(){
         cancelBtn.click();
