0e2639382cc1f6afd73d5eb5783dc4dce4916d74
Maksym Antoniuk <ul0278736@edu.uni.lodz.pl>
2026-02-26 19:44:32 +0100

    add profile update assertion and password update

--- src/main/java/io/maksym/web/pages/my_notes/MyNotesProfilePage.java
+++ src/main/java/io/maksym/web/pages/my_notes/MyNotesProfilePage.java
@@ -11,6 +11,7 @@ import io.maksym.web.records.ui.MyNoteUpdateUser;
 import io.qameta.allure.Step;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static io.maksym.web.enums.UiErrorMessage.SUCCESSFUL_PROFILE_UPDATE_MESSAGE;
 import static io.maksym.web.enums.UiModalTitle.DELETE_ACCOUNT_MODAL_TITLE;
 
 public class MyNotesProfilePage extends BasePage {
@@ -63,11 +64,6 @@ public class MyNotesProfilePage extends BasePage {
         companyNameField.fill(companyName);
     }
 
-    @Step("Assert User ID field is disabled")
-    public void assertUserIdFieldIsDisabled(){
-        assertThat(userIdField).isDisabled();
-    }
-
     @Step("Update profile data: Full Name, Phone Number, Company Name")
     public void updateProfile(MyNoteUpdateUser data){
         fillFullNameField(data.getFullName());
@@ -107,36 +103,49 @@ public class MyNotesProfilePage extends BasePage {
         return new MyNotesLoginPage(page);
     }
 
+    @Step("Click on Change password tab")
     public void clickChangePasswordTab(){
         changePasswordTab.click();
     }
+
+    @Step("Fill Current Password field")
     public void fillCurrentPasswordField(String password){
         currentPasswordField.fill(password);
     }
 
+    @Step("Fill New Password field")
     public void fillNewPasswordField(String password){
         newPasswordField.fill(password);
     }
+
+    @Step("Fill Confirm Password field")
     public void fillConfirmPasswordField(String password){
         confirmPasswordField.fill(password);
     }
+
+    @Step("Click on Update password button")
     public void clickUpdatePasswordButton(){
         updatePasswordButton.click();
     }
 
+    @Step("Update password")
     public void updatePassword(String currentPassword, String newPassword){
         clickChangePasswordTab();
         fillCurrentPasswordField(currentPassword);
         fillNewPasswordField(newPassword);
         fillConfirmPasswordField(newPassword);
         clickUpdatePasswordButton();
-        page.waitForTimeout(2000);
     }
 
+    @Step("Assert Password update message")
     public void assertPasswordUpdatedMessage(){
         alertToast.assertAlertToastIsVisible("The password was successfully updated");
     }
 
+    @Step("Assert successful Profile update message")
+    public void assertSuccessfulProfileUpdateMessage(){
+        alertToast.assertAlertToastIsVisible(SUCCESSFUL_PROFILE_UPDATE_MESSAGE.getMessage());
+    }
 
     @Override
     protected String path() {
